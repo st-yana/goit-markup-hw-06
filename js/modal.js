c3125e6791b8620796100ce5bd4cee205e5add83
Maksym Kovalenko <maksmaks5500@gmail.com>
2024-06-13 17:09:38 +0200

    fix modal

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-// document.querySelector('.modal__button-close').addEventListener('click', () => {
-//     document.querySelector('.backdrop').classList.remove('is-open');
-//   });
+document.querySelector('.modal__button-close').addEventListener('click', () => {
+    document.querySelector('.backdrop').classList.remove('is-open');
+  });
   
